1f2cdb6ebdc3e455a85f19bd78e88664b14d4e65
soohan <evepledge@gmail.com>
2024-12-09 16:04:06 +0900

    Feat: assets fonts 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      Jua: ["Jua"],
+      BagelFatOne: ["Bagel Fat One"],
+      Pretendard: ["Pretendard"],
+    },
   },
   plugins: [],
 };
