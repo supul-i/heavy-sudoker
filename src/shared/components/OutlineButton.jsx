a34ac779a60861279ec039f828a04d6b7c473d06
soohan <evepledge@gmail.com>
2024-12-11 16:37:31 +0900

    Feat: 홈 화면 버튼 클릭 이벤트함수  props 추가

--- src/shared/components/OutlineButton.jsx
+++ src/shared/components/OutlineButton.jsx
@@ -1,14 +1,18 @@
 import PropTypes from "prop-types";
 
-function OutlineButton({ text }) {
+function OutlineButton({ onClick = () => {}, text }) {
   return (
-    <button className="rounded-xl rounded-b-2xl border border-x-4 border-b-8 border-t-4 border-black px-7 py-3.5 text-3xl font-extrabold">
+    <button
+      className="rounded-xl rounded-b-2xl border border-x-4 border-b-8 border-t-4 border-black px-7 py-3.5 text-3xl font-extrabold dark:border-white dark:text-white"
+      onClick={onClick}
+    >
       {text}
     </button>
   );
 }
 
 OutlineButton.propTypes = {
+  onClick: PropTypes.func,
   text: PropTypes.string.isRequired,
 };
 
