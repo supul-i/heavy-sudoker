338aeb9ac2bb4830524d662917aa7cda8a61ab7a
soohan <evepledge@gmail.com>
2024-12-11 16:32:47 +0900

    Design: 홈 화면 스타일 수정

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import OutlineButton from "../shared/components/OutlineButton";
 
 function Home() {
   return (
-    <div className="px-4 dark:bg-black">
+    <div className="h-screen px-4 dark:bg-black">
       <div className="relative flex items-center justify-between pt-6 text-sm font-semibold leading-6 text-slate-700 lg:pt-8 dark:text-slate-200"></div>
       <div className="mx-auto max-w-5xl pt-60">
         <span className="font-tenada flex flex-col text-center text-4xl font-extrabold text-slate-900 sm:text-5xl lg:text-6xl dark:text-white">
