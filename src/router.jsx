70552814a1fbeb3e50d082fc061d20bb7b03cc6e
soohan <evepledge@gmail.com>
2024-12-11 16:49:17 +0900

    Feat: 스도쿠 진행 페이지 라우트 추가

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
+import SudokuPuzzles from "./components/SudokuPuzzles";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/sudoku",
+    element: <SudokuPuzzles />,
   },
 ]);
 
