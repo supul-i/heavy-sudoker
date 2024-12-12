6badea2caf20f5621d84868f8f05b91ca0fc42e4
soohan <evepledge@gmail.com>
2024-12-13 05:49:56 +0900

    Feat: 난이도에 맞게 퍼즐이 보여지도록 변경

--- src/components/SudokuGame/Board.jsx
+++ src/components/SudokuGame/Board.jsx
@@ -1,14 +1,18 @@
 import PropTypes from "prop-types";
 
-function Board({ sudokuMap }) {
+function Board({ sudokuMap, positionOfEmptyCell }) {
   return (
     <div className="grid h-[600px] w-[600px] grid-cols-9 grid-rows-9 gap-x-[2px] gap-y-[2px] bg-black p-2 text-center font-Pretendard font-semibold">
       {sudokuMap.map((numbersOfRow, rowIndex) => {
-        return numbersOfRow.map((number, colIndex) => (
-          <div className="cell" key={String(rowIndex) + String(colIndex)}>
-            {number}
-          </div>
-        ));
+        return numbersOfRow.map((number, colIndex) => {
+          return positionOfEmptyCell[rowIndex].includes(colIndex) ? (
+            <div className="cell" key={String(rowIndex) + String(colIndex)}></div>
+          ) : (
+            <div className="cell" key={String(rowIndex) + String(colIndex)}>
+              {number}
+            </div>
+          );
+        });
       })}
     </div>
   );
@@ -16,6 +20,7 @@ function Board({ sudokuMap }) {
 
 Board.propTypes = {
   sudokuMap: PropTypes.array.isRequired,
+  positionOfEmptyCell: PropTypes.array.isRequired,
 };
 
 export default Board;
--- src/components/SudokuGame/SudokuPuzzles.jsx
+++ src/components/SudokuGame/SudokuPuzzles.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import getEmptyCellsIndex from "../../utils/getEmptyCellsIndex";
 import setSudoku from "../../utils/setSudoku";
 import Board from "../SudokuGame/Board";
 import NumericKeypad from "../SudokuGame/NumericKeypad";
@@ -6,12 +7,13 @@ import NumericKeypad from "../SudokuGame/NumericKeypad";
 function SudokuPuzzles() {
   const location = useLocation();
   const difficultyLevel = location.state.difficulty;
+  const positionOfEmptyCell = getEmptyCellsIndex(difficultyLevel);
   const sudokuMap = setSudoku();
 
   return (
     <div className="grid h-screen place-items-center">
       <div className="grid grid-cols-2 place-items-center p-20">
-        <Board sudokuMap={sudokuMap} />
+        <Board sudokuMap={sudokuMap} positionOfEmptyCell={positionOfEmptyCell} />
         <NumericKeypad />
       </div>
     </div>
