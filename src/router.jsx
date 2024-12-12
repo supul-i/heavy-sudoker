9970635a2d5e5af18f97eb70aa56f2adef179970
soohan <evepledge@gmail.com>
2024-12-13 05:56:54 +0900

    Rename: SudokuGame 디렉토리로 이동

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
-import SudokuPuzzles from "./components/SudokuPuzzles";
+import SudokuPuzzles from "./components/SudokuGame/SudokuPuzzles";
 
 const router = createBrowserRouter([
   {
