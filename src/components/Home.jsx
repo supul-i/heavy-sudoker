465316bcddfb96fb6ef36290626002fb0569da3b
soohan <evepledge@gmail.com>
2024-12-13 05:35:23 +0900

    Style: 홈 화면 className 순서 변경

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
     <div className="h-screen px-4 dark:bg-black">
       <div className="relative flex items-center justify-between pt-6 text-sm font-semibold leading-6 text-slate-700 lg:pt-8 dark:text-slate-200"></div>
       <div className="mx-auto max-w-5xl pt-60">
-        <span className="font-tenada flex flex-col text-center text-4xl font-extrabold text-slate-900 sm:text-5xl lg:text-6xl dark:text-white">
+        <span className="flex flex-col text-center font-tenada text-4xl font-extrabold text-slate-900 sm:text-5xl lg:text-6xl dark:text-white">
           HEAVY SUDOKER
         </span>
         <div className="mt-24 flex justify-center space-x-8 font-Pretendard">
