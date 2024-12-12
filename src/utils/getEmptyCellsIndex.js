5cc7db97cbb58ef8f6f11444f614046c7fb84535
soohan <evepledge@gmail.com>
2024-12-13 05:47:52 +0900

    Feat: 난이도에 따른 스도쿠 퍼즐 로직 구현

---
+++ src/utils/getEmptyCellsIndex.js
@@ -0,0 +1,35 @@
+import { getRandomNum } from "./setSudoku";
+
+const PUZZLE_SIZE = 9;
+
+const setEmptyCell = (colIndexes) => {
+  const selectableIndexes = Array.from({ length: 9 }, (_, index) => index).filter(
+    (number) => !colIndexes.includes(number)
+  );
+  const randomIndex = getRandomNum(selectableIndexes.length);
+
+  colIndexes.push(selectableIndexes[randomIndex]);
+
+  return colIndexes;
+};
+
+const getEmptyCellsIndex = (difficultyLevel) => {
+  const numberOfEmptyCells =
+    difficultyLevel === "easy" ? 43 : difficultyLevel === "normal" ? 51 : 56;
+  let emptyCellPosition = Array.from({ length: 9 }, () => []);
+
+  for (let i = 0; i < numberOfEmptyCells; i++) {
+    const randomRowIndex = getRandomNum(PUZZLE_SIZE);
+
+    if (emptyCellPosition[randomRowIndex].length >= PUZZLE_SIZE - 1) {
+      i--;
+    } else {
+      const emptyCells = setEmptyCell(emptyCellPosition[randomRowIndex]);
+      emptyCellPosition[randomRowIndex] = emptyCells;
+    }
+  }
+
+  return emptyCellPosition;
+};
+
+export default getEmptyCellsIndex;
