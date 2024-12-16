ef8565037bdbbb8ab4aa4dd631ce83cfb8a0501c
soohan <evepledge@gmail.com>
2024-12-17 03:32:58 +0900

    Feat: 3DView 큐브 컴포넌트 생성 및 viewMode 전환 기능 추가

--- src/components/SudokuGame/SudokuPuzzles.jsx
+++ src/components/SudokuGame/SudokuPuzzles.jsx
@@ -1,19 +1,46 @@
-import { useLocation } from "react-router-dom";
-import getEmptyCellsIndex from "../../utils/getEmptyCellsIndex";
-import setSudoku from "../../utils/setSudoku";
+import usePuzzleStore from "../../store/usePuzzleStore";
 import Board from "../SudokuGame/Board";
 import NumericKeypad from "../SudokuGame/NumericKeypad";
+import CubeBoard from "./SudokuCube/CubeBoard";
+import CubeCell from "./SudokuCube/CubeCell";
 
 function SudokuPuzzles() {
-  const location = useLocation();
-  const difficultyLevel = location.state.difficulty;
-  const positionOfEmptyCell = getEmptyCellsIndex(difficultyLevel);
-  const sudokuMap = setSudoku();
+  const viewMode = usePuzzleStore((state) => state.viewMode);
+  const answerSudoku = usePuzzleStore((state) => state.answerSudoku);
+  const emptyCellPosition = usePuzzleStore((state) => state.emptyCellPosition);
+
+  const isEmpty = (layer, rowIndex, colIndex) => {
+    return emptyCellPosition[layer][rowIndex].includes(colIndex);
+  };
+
+  const getCubeBoard = () => {
+    const cubeBoard = [];
+
+    for (let x = 8; x >= 0; x--) {
+      for (let y = 8; y >= 0; y--) {
+        for (let z = 8; z >= 0; z--) {
+          cubeBoard.push(
+            CubeCell({
+              position: [x, y, z],
+              number: answerSudoku[8 - x][8 - y][8 - z],
+              isEmpty: isEmpty(8 - x, 8 - y, 8 - z),
+            })
+          );
+        }
+      }
+    }
+
+    return cubeBoard;
+  };
 
   return (
     <div className="grid h-screen place-items-center">
       <div className="grid grid-cols-2 place-items-center p-20">
-        <Board sudokuMap={sudokuMap} positionOfEmptyCell={positionOfEmptyCell} />
+        {viewMode === "threeDimensions" ? (
+          <CubeBoard cubeMap={getCubeBoard()} />
+        ) : (
+          <Board sudokuMap={answerSudoku} positionOfEmptyCell={emptyCellPosition} />
+        )}
         <NumericKeypad />
       </div>
     </div>
