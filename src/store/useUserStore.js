d2153ad303446d621cc929427cb325252db05e21
soohan <evepledge@gmail.com>
2024-12-18 11:14:48 +0900

    Feat: 3차원 스도쿠로 인한 사용자 입력 기록 전역상태 구조 변경

--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -3,7 +3,9 @@ import { immer } from "zustand/middleware/immer";
 import { PUZZLE_SIZE } from "../constants/puzzle";
 
 const initialState = {
-  userInputValues: Array.from({ length: PUZZLE_SIZE }, () => Array(PUZZLE_SIZE).fill(null)),
+  userInputValues: Array.from({ length: PUZZLE_SIZE }, () =>
+    Array.from({ length: PUZZLE_SIZE }, () => Array(PUZZLE_SIZE).fill(null))
+  ),
 };
 
 const useUserStore = create(
@@ -14,9 +16,9 @@ const useUserStore = create(
       set(() => ({
         userInputValues: userInputValues,
       })),
-    updateUserInputValue: (rowIndex, colIndex, newInputValue) =>
+    updateUserInputValue: (layer, rowIndex, colIndex, newInputValue) =>
       set((state) => {
-        state.userInputValues[rowIndex][colIndex] = newInputValue;
+        state.userInputValues[layer][rowIndex][colIndex] = newInputValue;
       }),
     resetUserHistory: () =>
       set(() => ({
