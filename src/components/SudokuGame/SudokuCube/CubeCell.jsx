f14e65417c714f0f9712ee5b897e01342fbaa0ba
soohan <evepledge@gmail.com>
2024-12-23 04:04:42 +0900

    Feat: 3D View 모드에서 셀 선택 시 해당 셀이 연동된 스도쿠 보드에 하이라이트 추가

--- src/components/SudokuGame/SudokuCube/CubeCell.jsx
+++ src/components/SudokuGame/SudokuCube/CubeCell.jsx
@@ -5,9 +5,11 @@ import usePuzzleStore from "../../../store/usePuzzleStore";
 
 function CubeCell({ position, number, isEmpty, userInputValue }) {
   const setCurrentLayer = usePuzzleStore((state) => state.setCurrentLayer);
+  const currentLayer = usePuzzleStore((state) => state.currentLayer);
   const cubeCell = useRef();
   const [hoveredCell, setHoveredCell] = useState(null);
   const isHovered = position.join() === hoveredCell;
+  const isHighlighted = position[0] === currentLayer;
 
   const handleSelectBoard = (e) => {
     e.stopPropagation();
@@ -87,6 +89,11 @@ function CubeCell({ position, number, isEmpty, userInputValue }) {
           {isEmpty ? null : number}
         </Text>
       </Billboard>
+      {isHighlighted && (
+        <Box args={[1, 1, 1]} position={[0.5, 0.5, -0.5]}>
+          <meshBasicMaterial transparent={true} opacity={0.5} color={"#39FF14"} />
+        </Box>
+      )}
     </group>
   );
 }
