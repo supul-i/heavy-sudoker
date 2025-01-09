94f0e6ef066aaab5dd77b5ff9552f8c3edc5d525
soohan <evepledge@gmail.com>
2025-01-09 10:31:17 +0900

    Remove: 적용되지 않는 기능 삭제

--- src/components/SudokuGame/NumericKeypad.jsx
+++ src/components/SudokuGame/NumericKeypad.jsx
@@ -15,14 +15,6 @@ function NumericKeypad() {
 
   return (
     <div className="h-80 w-56">
-      <div className="mb-3 grid grid-cols-2 gap-3">
-        <button className="rounded-lg bg-slate-900 px-4 py-2.5 text-sm font-semibold text-white hover:bg-slate-700">
-          메모
-        </button>
-        <button className="rounded-lg bg-slate-900 px-4 py-2.5 text-sm font-semibold text-white hover:bg-slate-700">
-          삭제
-        </button>
-      </div>
       <div className="grid grid-cols-3 grid-rows-3 gap-3 text-center">
         {numbersOfPad.map((number) => (
           <div
