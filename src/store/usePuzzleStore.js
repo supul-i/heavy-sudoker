937d638e1b9d50c34bb5978851b4aaca8a0d2806
soohan <kimsoohan.dev@gmail.com>
2025-04-06 02:45:39 +0900

    Feat: 테마 변경을 위한 전역상태 추가 및 초기화 함수 구현

--- src/store/usePuzzleStore.js
+++ src/store/usePuzzleStore.js
@@ -1,12 +1,24 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
+const getInitialTheme = () => {
+  const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const initialTheme = isDark ? "dark" : "light";
+
+  if (initialTheme === "light") {
+    document.documentElement.classList.remove("dark");
+  }
+
+  return initialTheme;
+};
+
 const initialState = {
   answerSudoku: [],
   emptyCellPosition: Array.from({ length: 9 }, () => []),
   currentCell: { row: 0, col: 0 },
   currentLayer: 8,
   completedBoards: Array.from({ length: 9 }, () => false),
+  theme: getInitialTheme(),
 };
 
 const usePuzzleStore = create(
@@ -43,6 +55,17 @@ const usePuzzleStore = create(
           const newCompletedBoards = [...state.completedBoards];
           newCompletedBoards[boardLayer] = true;
           return { completedBoards: newCompletedBoards };
+        }),
+
+      setTheme: (newTheme) =>
+        set(() => {
+          if (newTheme === "dark") {
+            document.documentElement.classList.add("dark");
+          } else {
+            document.documentElement.classList.remove("dark");
+          }
+
+          return { theme: newTheme };
         }),
 
       resetPuzzle: () =>
