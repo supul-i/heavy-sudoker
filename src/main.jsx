c9e8ac02e31f6a3e688ef2a307200ce21e60daf6
soohan <kimsoohan.dev@gmail.com>
2025-04-11 08:56:02 +0900

    Feat: 모든 페이지에 화면 크기 제한 기능 적용

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import ScreenRestrictionLayout from "./shared/components/ScreenRestrictionLayout";
 import "./shared/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ScreenRestrictionLayout>
+      <RouterProvider router={router} />
+    </ScreenRestrictionLayout>
   </StrictMode>
 );
