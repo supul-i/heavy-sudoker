2fc530f8d79c58ee8e7e9cb2714eb516ea3892a2
soohan <kimsoohan.dev@gmail.com>
2025-04-11 08:45:41 +0900

    Feat: 안내메시지 컴포넌트 추가

---
+++ src/shared/components/GuideMessage.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+function GuideMessage({ message }) {
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <div className="flex flex-auto flex-col items-center justify-center px-4 text-center sm:flex-row">
+          <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:mr-6 sm:border-r sm:border-gray-900/10 sm:pr-6 sm:text-3xl dark:text-gray-200 sm:dark:border-gray-300/10">
+            이용 안내
+          </h1>
+          <h2 className="mt-2 text-gray-700 sm:mt-0 dark:text-gray-400">{message}</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+GuideMessage.propTypes = {
+  message: PropTypes.string,
+};
+
+export default GuideMessage;
