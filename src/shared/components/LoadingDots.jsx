11cf0c87a1ac2c7462fdcad9164586900234cfee
soohan <kimsoohan.dev@gmail.com>
2025-04-13 22:34:16 +0900

    Design: 로딩 컴포넌트 UI 수정

--- src/shared/components/LoadingDots.jsx
+++ src/shared/components/LoadingDots.jsx
@@ -1,6 +1,6 @@
 function LoadingDots() {
   return (
-    <div className="flex justify-center space-x-2">
+    <div className="flex min-h-screen w-full items-center justify-center space-x-2">
       <div className="h-10 w-10 flex-none animate-bounce rounded-full bg-slate-900 [animation-delay:-0.6s] dark:bg-white"></div>
       <div className="h-10 w-10 flex-none animate-bounce rounded-full bg-slate-900 [animation-delay:-0.3s] dark:bg-white"></div>
       <div className="h-10 w-10 flex-none animate-bounce rounded-full bg-slate-900 [animation-delay:-0.15s] dark:bg-white"></div>
